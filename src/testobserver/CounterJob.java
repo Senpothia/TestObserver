cc5b1ceeaa0eb8e424397aaff25ed6fee7d7b7aa
Michel Lopez <michel@cdvi.com>
2021-03-12 15:48:03 +0100

    Implémenation classe Interface

---
+++ src/testobserver/CounterJob.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testobserver;
+
+import java.util.Observable;
+
+/**
+ *
+ * @author michel
+ */
+public class CounterJob extends Observable implements Runnable {
+    
+        private int count;
+    
+	public CounterJob() {
+		super();
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+	this.count = count;
+	this.setChanged();
+	this.notifyObservers(this.getCount());
+        }
+
+    @Override
+    public void run() {
+        
+        try {
+		while (this.getCount() < 1000) {
+			this.setCount(this.getCount() + 1);
+                        System.out.println("Valeur compteur: " + this.getCount());
+			Thread.sleep(1500);
+		}
+	} catch (InterruptedException e) {
+	}
+        
+    }
+
+    void addObserver(int compteur) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- src/testobserver/TestObserver.java
+++ src/testobserver/TestObserver.java
@@ -16,30 +16,11 @@ public class TestObserver {
      */
     public static void main(String[] args) {
        
-           /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
+        /*
+       Thread thread = new Thread(new CounterJob());
+       thread.start();
+       */
+        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Interface().setVisible(true);
